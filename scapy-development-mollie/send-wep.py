81db2808f446b2965aaad8b18df09e4935309996
Mollie <mollie.e.murray@gmail.com>
2020-04-30 01:53:36 -0400

    organize folder and add to write-pcap.py

--- scapy-development-mollie/send-wep.py
+++ scapy-development-mollie/send-wep.py
@@ -36,8 +36,10 @@ sendp(encPkt)
 #Sending a Dot11 Beacon packet
 SSID = 'Test SSID'
 iface = 'eth0'
-dot11 = Dot11(type=0, subtype=8, addr1='ff:ff:ff:ff:ff:ff',
-addr2=sender, addr3=sender)
+dot11 = Dot11(type=0, subtype=8, 
+        addr1='ff:ff:ff:ff:ff:ff',
+        addr2=sender,
+        addr3=sender)
 beacon = Dot11Beacon()
 essid = Dot11Elt(ID='SSID',info=SSID, len=len(SSID))
 frame = RadioTap()/dot11/beacon/essid
